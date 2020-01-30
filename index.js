93b99a7c65cb3bef6d86e9020b928449b571b7d5
Heedo Kim(Choco) on Mac Mini <devheedoo@gmail.com>
2020-01-30 17:00:16 +0900

    Test success
    - Use require instead of import
    - TypeScript type works well (test only in VSCode)

---
+++ index.js
@@ -0,0 +1,6 @@
+// import OneClassExporting from "one-class-exporting";
+const OneClassExporting = require('one-class-exporting').default;
+
+const oneClassExporting = new OneClassExporting('Hello', 13);
+
+console.log(oneClassExporting.toString());
